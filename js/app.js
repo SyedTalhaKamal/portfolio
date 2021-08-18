d29c07d71985ad82e57923a88c1505665951a491
SyedTalhaKamal <syedtalhakamal22@gmail.com>
2021-08-18 12:45:34 +0500

    Portfolio

--- js/app.js
+++ js/app.js
@@ -1,31 +1,4 @@
-// ========================================= Owl carousel ============================================================//
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    dots:true,
-    center:false,
-    navText : ["<img src='./images/back.png' class='arrows'>","<img src='./images/forword.png' class='arrows'>"],
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:3
-        }
-    }
-})
+$(document).ready(function () {
+  AOS.init();
+});
 
-    $(document).ready(function () {
-
-        setTimeout(function () {
-          $('.preloader').fadeOut(100);
-        }, 1000);
-    
-      });
-
-
-      new WOW().init();
